f9d2df02b14bf54ff4affab76fbfb2b9771a3a32
Maciej Bukowski <ma2ciek@gmail.com>
2015-02-07 15:49:53 +0100

    Succesful installation of node.js

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var routes = require('./routes');
+var methodOverride = require('method-override');
+
+var app = module.exports = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.set('view options', {
+	layout: false
+});
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
+app.use(bodyParser.json());
+app.use(methodOverride())
+app.use(express.static(__dirname + '/pub'));
+
+app.get('/', routes.index);
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+	console.log("Listening on %d", port);
+});
