6ebd0d12b4b10c55719a623c9799692894b8341c
Maciej Bukowski <ma2ciek@gmail.com>
2015-02-08 01:35:31 +0100

    Ultra hiper super - możliwość dodania wielu graczy do gry

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var bodyParser = require('body-parser');
 var routes = require('./routes');
 var methodOverride = require('method-override');
@@ -23,23 +25,247 @@ app.get('/', routes.index);
 
 var port = process.env.PORT || 8080;
 
-io.on('connection', function(socket){
-  	socket.broadcast.emit('n-message', 'Nowa osoba dołączyła do rozmowy');
-  	socket.on('disconnect', function(){
-    	io.emit('n-message', 'Osoba się rozłączyła');
+
+io.on('connection', function(socket) {
+	socket.broadcast.emit('n-message', 'Nowa osoba dołączyła do rozmowy');
+
+	console.log(socket.id);
+	tank.create(socket.id);
+
+	socket.on('disconnect', function() {
+		io.emit('n-message', 'Osoba się rozłączyła');
+		if(!delete tank.list[socket.id]) {
+			console.log('ERROR - nie można usunąć czołgu');
+		}
 	});
-	socket.on('message', function(msg){
+	socket.on('message', function(msg) {
 		socket.broadcast.emit('message', msg);
 	});
+	socket.on('client-event', function(msg) {
+
+		var id = socket.id;
+
+		for (var i in msg) {
+			switch (i) {
+				case 'mx':
+					tank.list[id]['mx'] = msg[i];
+					break;
+				case 'my':
+					tank.list[id]['my'] = msg[i];
+					break;
+				case 'shot':
+					tank.list[id].shot();
+					break;
+				case 'dirX':
+					tank.list[id]['dirX'] = msg[i];
+					break;
+				case 'dirY':
+					tank.list[id]['dirY'] = msg[i];
+					break;
+				default:
+					console.log(i, msg[i]);
+					break;
+			}
+		}
+	})
 });
 setInterval(function() {
 	var clients = io.engine.clientsCount
-	if(clients == 1) clients += ' person connected';
+	if (clients == 1) clients += ' person connected';
 	else clients += ' persons connected'
 	io.emit('clients', clients);
 }, 1000);
 
+setInterval(gameLoop, 20);
 
 http.listen(port, function() {
 	console.log(port);
 });
+
+
+function gameLoop() {
+	tank.move();
+	bullets.move();
+	send_data();
+}
+
+function send_data() {
+	var res = JSON.stringify({
+		tank: tank.list,
+		bullets: bullets.list
+	});
+	io.emit('game-update', res);
+}
+
+var board = {
+	WIDTH: 1000,
+	HEIGHT: 500,
+	elems: [{
+		type: 'box',
+		x1: 200,
+		y1: 200,
+		x2: 264,
+		y2: 264,
+		life: 10
+	}],
+}
+
+var tank = {
+	list: {},
+	proto: function(id) {
+		this.x = 100;
+		this.y = 100;
+		this.speed = 6;
+		this.dirX = 0;
+		this.dirY = 0;
+		this.radius = 22;
+		this.lufa = {
+			x1: 0,
+			y1: 0,
+			x2: 0,
+			y2: 0
+		};
+		this.id = id;
+		this.life = 100;
+		this.shot = function() {
+			bullets.create(this.id);
+		}
+	},
+	create: function(id) {
+		tank.list[id] = new tank.proto(id);
+	},
+	move: function() {
+		for (var id in tank.list) {
+
+			var r = tank.list[id]['radius'];
+			var x = tank.list[id]['x'];
+			var y = tank.list[id]['y'];
+
+			var speed = tank.list[id]['speed'];
+
+			var dx = tank.list[id]['dirX'];
+			var dy = tank.list[id]['dirY'];
+
+			if (dx != 0 && dy != 0) speed /= 1.4;
+
+			x += dx * speed;
+			y += dy * speed;
+
+			// Kolizje ze ścianami
+			if (x < r) {
+				x = r;
+			} else if (x > board.WIDTH - r) {
+				x = board.WIDTH - r;
+			}
+			if (y < r) {
+				y = r;
+			} else if (y > board.HEIGHT - r) {
+				y = board.HEIGHT - r;
+			}
+
+			// Kolizje z boksami
+
+			for (var i = 0; i < board.elems.length; i++) {
+				var b = board.elems[i];
+				if (b.type != 'box') continue;
+
+				if (b.x1 < x + r && b.x2 > x - r && b.y1 < y + r && b.y2 > y - r) {
+
+					if (dx == -1) {
+						if (Math.abs(b.x2 - (x - r)) <= Math.abs(dx * speed)) { // kolizja z prawym bokiem boxu
+							x = b.x2 + r;
+						}
+					} else if (dx == 1) {
+						if (Math.abs(b.x1 - (x + r)) <= Math.abs(dx * speed)) { // kolizja z lewym bokiem boxu
+							x = b.x1 - r;
+						}
+					}
+
+					if (dy == -1) {
+						if (Math.abs(b.y2 - (y - r)) <= Math.abs(dy * speed)) { // kolizja z prawym bokiem boxu
+							y = b.y2 + r;
+						}
+					} else if (dy == 1) {
+						if (Math.abs(b.y1 - (y + r)) <= Math.abs(dy * speed)) { // kolizja z lewym bokiem boxu
+							y = b.y1 - r;
+						}
+					}
+
+				}
+			}
+
+			tank.list[id].x = x;
+			tank.list[id].y = y;
+
+
+			var ta = tank.list[id];
+			var v = new vector(ta['mx'] - ta['x'], ta['my'] - ta['y']);
+			ta['lufa'] = {
+				x1: ta['x'] + v.unit.x * 8,
+				y1: ta['y'] + v.unit.y * 8,
+				x2: ta['x'] + v.unit.x * 30,
+				y2: ta['y'] + v.unit.y * 30
+			};
+		}
+	}
+};
+var bullets = {
+	list: [],
+	move: function() {
+		for (var i = 0; i < bullets.list.length; i++) {
+			var b = bullets.list[i];
+			b.x += b.sx * 10;
+			b.y += b.sy * 10;
+
+			if (b.x < 0 || b.y < 0 || b.x > board.WIDTH || b.y > board.HEIGHT) {
+				bullets.list.splice(i, 1);
+				i--;
+				continue;
+			}
+			for (var j = 0; j < board.elems.length; j++) {
+				var e = board.elems[j];
+				if (b.x + b.r > e.x1 && b.x - b.r < e.x2 && b.y + b.r > e.y1 && b.y - b.r < e.y2) {
+					// e.life--;
+					// if (e.life <= 0) {
+					//	bg_context.clearRect(e.x1, e.y1, e.y1, e.y2);
+					//	board.elems.splice(j, 1);
+					// }
+					bullets.list.splice(i, 1);
+					i--;
+					break;
+
+				}
+			}
+		}
+	},
+	proto: function(id) {
+		var x = tank.list[id].x;
+		var y = tank.list[id].y;
+		var mx = tank.list[id].mx;
+		var my = tank.list[id].my;
+
+		this.x = tank.list[id].lufa.x2;
+		this.y = tank.list[id].lufa.y2;
+
+		var size = Math.sqrt((mx - x) * (mx - x) + (my - y) * (my - y));
+
+		this.sx = (mx - x) / size;
+		this.sy = (my - y) / size;
+
+		this.r = 5;
+	},
+	create: function(id) {
+		bullets.list.push(new bullets.proto(id));
+	},
+}
+
+// prototyp wektora
+var vector = function(x, y) {
+	this.x = x;
+	this.y = y;
+	this.size = Math.sqrt(x * x + y * y);
+	this.unit = {
+		x: this.x / this.size,
+		y: this.y / this.size
+	};
+}
--- pub/main.js
+++ pub/main.js
@@ -4,42 +4,49 @@ var game_context;
 var bg_context;
 var PI = Math.PI;
 var socket = io();
-
+var id = 0;
 
 // CHAT
 function submit() {
-	if($('#m').val() == "") return;
+	if ($('#m').val() == "") return;
 	socket.emit('message', $('#m').val());
 	$('#messages').append('<li class="my"><span>' + $('#m').val() + '</span></li>');
 	$('#m').val('');
 }
+$(window).on('beforeunload', function() {
+	socket.disconnect()
+});
+
 socket.on('message', function(msg) {
-	$('#messages').append('<li><span>' +  msg + '</span></li>');
+	$('#messages').append('<li><span>' + msg + '</span></li>');
+
 });
 socket.on('n-message', function(msg) {
-	$('#messages').append('<li class="neutral"><span>' +  msg + '</span></li>');
+	$('#messages').append('<li class="neutral"><span>' + msg + '</span></li>');
 });
 socket.on('clients', function(msg) {
 	$('#clients').text(msg);
 });
 
-
-
 window.addEventListener('load', function load() {
 	game_context = $('#game')[0].getContext('2d');
 	bg_context = $('#bg')[0].getContext('2d');
-	game.timer.start = +new Date();
 	events();
 	board.create();
-	game.draw();
+
+	socket.on('game-update', function(msg) {
+		id = socket.id;
+		var msg = JSON.parse(msg);
+		tank.list = msg.tank;
+		bullets.list = msg.bullets;
+		game.draw();
+	});
+	
 });
 
 var board = {
 	WIDTH: 1000,
 	HEIGHT: 500,
-	proto: function(x, y, type, src) {
-		// do dodania
-	},
 	elems: [{
 		type: 'box',
 		x1: 200,
@@ -78,161 +85,64 @@ var game = {
 			var a = new Audio('gun_shot.wav');
 			a.volume = 0.5;
 			return a;
+		})(),
+		message: (function() {
+			var a = new Audio();
 		})()
 	},
 	context: null,
 	counter: 0,
-	timer: {
-		T_BREAK: 30,
-		start: null,
-		id: null,
-		t_buff: 0,
-		remaining: function() {
-			var diff = +new Date() - this.start;
-			var delay = diff % this.T_BREAK;
-			var remaining = this.T_BREAK - delay;
-			return remaining;
-		}
-	},
 
 	draw: function() {
-
 		game.clearboard();
-
-		tank.move();
 		tank.draw();
-
-		bullets.move();
 		bullets.draw();
-
-		window.requestAnimationFrame(game.draw)
 	},
 	clearboard: function() {
 		var context = game_context;
 		context.clearRect(0, 0, board.WIDTH, board.HEIGHT);
 	}
 }
-var player = {
-	attr: {
-		'mx': 0,
-		'my': 0,
-	}
-};
 
 var tank = {
-	attr: {
-		x: 100,
-		y: 100,
-		speed: 6,
-		dirX: 0,
-		dirY: 0,
-		radius: 22,
-		lufa: {
-			x1: 0,
-			y1: 0,
-			x2: 0,
-			y2: 0
-		},
-		life: 100,
-	},
-	move: function() {
-
-		var r = this.attr['radius'];
-		var x = this.attr['x'];
-		var y = this.attr['y'];
-
-		var speed = this.attr['speed'];
-
-		var dx = this.attr['dirX'];
-		var dy = this.attr['dirY'];
-
-		if (dx != 0 && dy != 0) speed /= 1.4;
-
-		x += dx * speed;
-		y += dy * speed;
-
-		// Kolizje ze ścianami
-		if (x < r) {
-			x = r;
-		} else if (x > board.WIDTH - r) {
-			x = board.WIDTH - r;
-		}
-		if (y < r) {
-			y = r;
-		} else if (y > board.HEIGHT - r) {
-			y = board.HEIGHT - r;
-		}
-
-		// Kolizje z boksami
-
-		for (var i = 0; i < board.elems.length; i++) {
-			var b = board.elems[i];
-			if (b.type != 'box') continue;
-
-			if (b.x1 < x + r && b.x2 > x - r && b.y1 < y + r && b.y2 > y - r) {
-
-				if (dx == -1) {
-					if (Math.abs(b.x2 - (x - r)) <= Math.abs(dx * speed)) { // kolizja z prawym bokiem boxu
-						x = b.x2 + r;
-					}
-				} else if (dx == 1) {
-					if (Math.abs(b.x1 - (x + r)) <= Math.abs(dx * speed)) { // kolizja z lewym bokiem boxu
-						x = b.x1 - r;
-					}
-				}
-
-				if (dy == -1) {
-					if (Math.abs(b.y2 - (y - r)) <= Math.abs(dy * speed)) { // kolizja z prawym bokiem boxu
-						y = b.y2 + r;
-					}
-				} else if (dy == 1) {
-					if (Math.abs(b.y1 - (y + r)) <= Math.abs(dy * speed)) { // kolizja z lewym bokiem boxu
-						y = b.y1 - r;
-					}
-				}
-
-			}
-		}
-
-		this.attr['x'] = x;
-		this.attr['y'] = y;
-	},
+	list: {},
 	shot: function() {
-		bullets.create();
 		game.audio.shot.cloneNode().play();
 	},
 	'draw': function() {
-		var ctx = game_context
-		var pa = player.attr;
-		var ta = tank.attr;
 
-		ctx.beginPath();
-		ctx.arc(ta['x'], ta['y'], 20, 0, 2 * PI, false);
-		ctx.strokeStyle = '#333';
-		ctx.lineWidth = 3;
-		ctx.stroke();
-		ctx.closePath();
+		for (var i in tank.list) {
+			var ctx = game_context;
+			var ta = tank.list[i];
 
-		ctx.beginPath();
-		ctx.arc(ta['x'], ta['y'], 10, 0, 2 * PI, false);
-		ctx.fillStyle = '#333';
-		ctx.fill();
-		ctx.closePath();
+			if(id == i) {
+				ctx.strokeStyle = '#333';
+			}
+			else {
+				ctx.strokeStyle = 'red';
+			}
 
-		var v = new vector(pa['mx'] - ta['x'], pa['my'] - ta['y']);
-		ta['lufa'] = {
-			x1: ta['x'] + v.unit.x * 8,
-			y1: ta['y'] + v.unit.y * 8,
-			x2: ta['x'] + v.unit.x * 30,
-			y2: ta['y'] + v.unit.y * 30
-		};
+			ctx.beginPath();
+			ctx.arc(ta['x'], ta['y'], 20, 0, 2 * PI, false);
+			
+			ctx.lineWidth = 3;
+			ctx.stroke();
+			ctx.closePath();
 
-		ctx.beginPath();
-		ctx.moveTo(ta['lufa'].x1, ta['lufa'].y1);
-		ctx.lineWidth = 6;
-		ctx.lineTo(ta['lufa'].x2, ta['lufa'].y2);
-		ctx.stroke();
-		ctx.closePath();
+			ctx.beginPath();
+			ctx.arc(ta['x'], ta['y'], 10, 0, 2 * PI, false);
+			ctx.fillStyle = '#333';
+			ctx.fill();
+			ctx.closePath();
+
+			ctx.beginPath();
+			ctx.moveTo(ta['lufa'].x1, ta['lufa'].y1);
+			ctx.lineWidth = 6;
+			ctx.lineTo(ta['lufa'].x2, ta['lufa'].y2);
+			ctx.stroke();
+			ctx.closePath();
+
+		}
 	}
 }
 
@@ -249,19 +159,27 @@ function events() {
 		switch (e.which) {
 			case 37: // LEFT
 			case 65: // A
-				tank.attr['dirX'] = -1;
+				socket.emit('client-event', {
+					dirX: "-1"
+				});
 				break;
 			case 38: // UP
 			case 87: // W
-				tank.attr['dirY'] = -1;
+				socket.emit('client-event', {
+					dirY: -1
+				});
 				break;
 			case 39: // RIGHT
 			case 68: // D
-				tank.attr['dirX'] = 1;
+				socket.emit('client-event', {
+					dirX: 1
+				});
 				break;
 			case 40: // DOWN
 			case 83: // S
-				tank.attr['dirY'] = 1;
+				socket.emit('client-event', {
+					dirY: 1
+				});
 				break;
 
 			case 13: // Enter - czat
@@ -278,13 +196,17 @@ function events() {
 			case 65: // A
 			case 39: // RIGHT
 			case 68: // D
-				tank.attr['dirX'] = 0;
+				socket.emit('client-event', {
+					dirX: 0
+				});
 				break;
 			case 38: // UP
 			case 87: // W
 			case 40: // DOWN
 			case 83: // S
-				tank.attr['dirY'] = 0;
+				socket.emit('client-event', {
+					dirY: 0
+				});
 				break;
 			default:
 		}
@@ -292,79 +214,25 @@ function events() {
 	var can = $('#game')[0];
 	can.addEventListener('mousemove', function(evt) {
 		var rect = can.getBoundingClientRect();
-		player.attr['mx'] = evt.clientX - rect.left;
-		player.attr['my'] = evt.clientY - rect.top;
+		socket.emit('client-event', {
+			mx: evt.clientX - rect.left
+		});
+		socket.emit('client-event', {
+			my: evt.clientY - rect.top
+		});
 	});
 	can.addEventListener('click', function(evt) {
 		tank.shot();
+		socket.emit('client-event', {
+			shot: true
+		});
 	});
 }
 
-// prototyp wektora
-var vector = function(x, y) {
-	var that = this;
-	this.x = x;
-	this.y = y;
-	this.size = Math.sqrt(x * x + y * y);
-	this.unit = {
-		x: that.x / that.size,
-		y: that.y / that.size
-	};
-}
+
 
 var bullets = {
 	list: [],
-	proto: function() {
-		var pa = player.attr;
-		var ta = tank.attr;
-		var x = tank.attr['x'];
-		var y = tank.attr['y'];
-		var mx = player.attr['mx'];
-		var my = player.attr['my'];
-
-		var that = this;
-		this.x = tank.attr['lufa'].x2;
-		this.y = tank.attr['lufa'].y2;
-
-		var size = Math.sqrt((mx - x) * (mx - x) + (my - y) * (my - y));
-
-		this.sx = (mx - x) / size;
-		this.sy = (my - y) / size;
-
-		this.r = 5;
-	},
-	create: function() {
-		bullets.list.push(new bullets.proto());
-	},
-	move: function() {
-		for (var i = 0; i < bullets.list.length; i++) {
-			var b = bullets.list[i];
-			b.x += b.sx * 10;
-			b.y += b.sy * 10;
-
-			if (b.x < 0 || b.y < 0 || b.x > board.WIDTH || b.y > board.HEIGHT) {
-				bullets.list.splice(i, 1);
-				i--;
-				continue;
-			}
-			for (var j = 0; j < board.elems.length; j++) {
-				var e = board.elems[j];
-				if (b.x + b.r > e.x1 && b.x - b.r < e.x2 && b.y + b.r > e.y1 && b.y - b.r < e.y2) {
-					e.life--;
-					if (e.life <= 0) {
-						bg_context.clearRect(e.x1, e.y1, e.y1, e.y2);
-						board.elems.splice(j, 1);
-					}
-					bullets.list.splice(i, 1);
-					i--;
-					break;
-
-				}
-			}
-
-
-		}
-	},
 	draw: function() {
 		var context = game_context;
 		for (var i = 0; i < bullets.list.length; i++) {
