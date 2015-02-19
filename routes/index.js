94c2a1dc3eabd8b26010e57f312b221ddf89b27a
Maciej Bukowski <ma2ciek@gmail.com>
2015-02-19 05:14:19 +0100

    Hotfix

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ exports.index = function(req, res) {
 exports.settings = function(req, res) {
 	res.render('settings');
 };
-exports.game = function(req, res) {
+exports.play = function(req, res) {
 	res.render('game');
 };
 exports.tutorial = function(req, res) {
 	res.render('tutorial');
 }
 exports.settings2 = function(req, res) {
-	
+
 }
