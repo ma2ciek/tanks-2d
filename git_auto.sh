05bb633516f3249c44e2513bc70bf2abb8fa7682
Maciej Bukowski <ma2ciek@gmail.com>
2015-02-23 01:09:51 +0100

    2-gi test

--- git_auto.sh
+++ git_auto.sh
@@ -4,5 +4,10 @@ echo 'Enter the commit message:'
 read commitMessage
 git commit -m "$commitMessage"
 
-git log-json > logs.json
-git commit --ammend --no-edit
+git log \
+    --pretty=format:'{%n  "commit": "%H",%n  "author": "%an <%ae>",%n  "date": "%ad",%n  "message": "%s"%n},' \
+    $@ | \
+    perl -pe 'BEGIN{print "["}; END{print "]\n"}' | \
+    perl -pe 's/},]/}]/' > logs.json
+    
+git commit --amend --no-edit
